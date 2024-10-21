843e6cbd5d142c9f09527bce829563aa42153721
Akhil Tadiparthi <akta1203@colorado.edu>
2024-10-21 12:15:25 -0600

    my pages

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Add Google Font
+const link = document.createElement('link');
+link.href = 'https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap';
+link.rel = 'stylesheet';
+document.head.appendChild(link);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
