adfd25e748345fecbfc06627293eb71d878d6750
Akhil Tadiparthi <akta1203@colorado.edu>
2024-10-21 16:21:20 -0600

    navbar changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,24 +6,21 @@ import { Link } from "react-router-dom";
 
 const NavBar = () => {
     return (
-        <div class="navbar">
-            {/* <div class="navbar-left">Logo</div> */}
-            <div class="navbar-center">
-            <h1 class="navbar-name">Akhil Tadiparhi's Portfolio</h1>
-                <ul class="nav-links">
-                    {/* <li><a href="/">Home</a></li>
-                    <li><a href="/resume">Resume</a></li>
-                    <li><a href="/projects">Projects</a></li>
-                    <li><a href="/contact">Contact Me</a></li> */}
-                    <Link to="/">Home</Link>
-                    <Link to="/resume">Resume</Link>
-                    <Link to="/projects">Projects</Link>
-                    <Link to="/contact">Contact Me</Link>
+        <div className="navbar">
+            {/* <div className="navbar-left">Logo</div> */}
+            <div className="navbar-center">
+                <h1 className="navbar-name">Akhil Tadiparhi's Portfolio</h1>
+                <ul className="nav-links">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/resume">Resume</Link></li>
+                    <li><Link to="/projects">Projects</Link></li>
+                    <li><Link to="/contact">Contact Me</Link></li>
                 </ul>
             </div>
-            {/* <div class="navbar-right">Profile</div> */}
+            {/* <div className="navbar-right">Profile</div> */}
         </div>
     );
 };
 
 export default NavBar;
+
